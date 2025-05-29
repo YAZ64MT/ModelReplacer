bb537d6baa381fcb36b3b82184abfabaaee90f21
Neirn <neirny@gmail.com>
2025-05-29 15:59:00 -0700

    Create zproxy
    Struct that represents a single vanilla object with a given object id and its currently handled display lists

---
+++ include/zproxy.h
@@ -0,0 +1,30 @@
+#ifndef __ZPROXY_H__
+#define __ZPROXY_H__
+
+#include "global.h"
+#include "recompdata.h"
+#include "z64object.h"
+#include "linkedlist.h"
+
+typedef struct {
+    Gfx* vanillaDisplayList;
+    Gfx displayList;
+} ZProxy_ProxyContainer;
+
+typedef struct {
+    ObjectId vanillaObjId;
+    U32MemoryHashmapHandle vanillaDLToCustomDLMap;
+    LinkedList vanillaDisplayLists;
+} ZProxy;
+
+void ZProxy_initZProxy(ZProxy *zProxy, ObjectId id);
+
+void ZProxy_destroyZProxy(ZProxy *zProxy);
+
+bool ZProxy_setCustomDisplayList(ZProxy *zProxy, Gfx *vanillaDisplayList, Gfx customDisplayList[]);
+
+bool ZProxy_removeCustomDisplayList(ZProxy *zProxy, Gfx *vanillaDisplayList);
+
+bool ZProxy_reserveContainer(ZProxy *zProxy, Gfx *vanillaDisplayList);
+
+#endif
---
+++ src/zproxy.c
@@ -0,0 +1,69 @@
+#include "global.h"
+#include "recomputils.h"
+#include "zproxy.h"
+#include "globalobjects_api.h"
+
+void ZProxy_initZProxy(ZProxy *this, ObjectId id) {
+    this->vanillaObjId = id;
+
+    this->vanillaDLToCustomDLMap = recomputil_create_u32_memory_hashmap(sizeof(ZProxy_ProxyContainer));
+
+    LinkedList_initList(&this->vanillaDisplayLists);
+}
+
+void ZProxy_destroyZProxy(ZProxy *this) {
+    recomputil_destroy_u32_memory_hashmap(this->vanillaDLToCustomDLMap);
+}
+
+bool ZProxy_reserveContainer(ZProxy *this, Gfx *vanillaDisplayList) {
+    u32 vanilla = (uintptr_t)vanillaDisplayList;
+
+    ZProxy_ProxyContainer *container = recomputil_u32_memory_hashmap_get(this->vanillaDLToCustomDLMap, vanilla);
+
+    if (!container) {
+        if (recomputil_u32_memory_hashmap_create(this->vanillaDLToCustomDLMap, vanilla)) {
+
+            LinkedList_addBack(&this->vanillaDisplayLists, vanillaDisplayList);
+
+            container = recomputil_u32_memory_hashmap_get(this->vanillaDLToCustomDLMap, vanilla);
+
+            container->vanillaDisplayList = vanillaDisplayList;
+            
+            gSPBranchList(&container->displayList, ZGlobalObj_getGlobalGfxPtr(this->vanillaObjId, vanillaDisplayList));
+
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool ZProxy_setCustomDisplayList(ZProxy *this, Gfx *vanillaDisplayList, Gfx customDisplayList[]) {
+    ZProxy_reserveContainer(this, vanillaDisplayList);
+
+    uintptr_t vanilla = (uintptr_t)vanillaDisplayList;
+
+    ZProxy_ProxyContainer *container = recomputil_u32_memory_hashmap_get(this->vanillaDLToCustomDLMap, vanilla);
+
+    if (container) {
+        gSPBranchList(&container->displayList, customDisplayList);
+        return true;
+    }
+
+    return false;
+}
+
+bool ZProxy_removeCustomDisplayList(ZProxy *this, Gfx *vanillaDisplayList) {
+    ZProxy_reserveContainer(this, vanillaDisplayList);
+
+    uintptr_t vanilla = (uintptr_t)vanillaDisplayList;
+
+    ZProxy_ProxyContainer *container = recomputil_u32_memory_hashmap_get(this->vanillaDLToCustomDLMap, vanilla);
+
+    if (container) {
+        gSPBranchList(&container->displayList, ZGlobalObj_getGlobalGfxPtr(this->vanillaObjId, vanillaDisplayList));
+        return true;
+    }
+
+    return false;
+}
